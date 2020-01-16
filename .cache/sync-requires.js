2cf95b79778a841bcf1aaf102e047b15fc2ba24d
Edwin Zamudio <ezamudio@hawk.iit.edu>
2020-01-15 22:29:19 -0600

    okie

---
+++ .cache/sync-requires.js
@@ -0,0 +1,17 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\node_modules\\gatsby-plugin-offline\\app-shell.js"))),
+  "component---src-templates-post-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\templates\\post.js"))),
+  "component---src-templates-tag-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\templates\\tag.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\pages\\404.js"))),
+  "component---src-pages-archive-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\pages\\archive.js"))),
+  "component---src-pages-blog-index-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\pages\\Blog\\index.js"))),
+  "component---src-pages-blog-tags-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\pages\\Blog\\tags.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Edwin Zamudio\\Documents\\GitHub\\notedwin.github.io\\src\\pages\\index.js")))
+}
+
